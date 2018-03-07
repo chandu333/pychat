6796e6e330078906081ca876d09f6914aa135816
chandu333 <33688870+chandu333@users.noreply.github.com>
2018-03-07 15:23:55 +0530

    Add files via upload

---
+++ client1.py
@@ -0,0 +1,47 @@
+import socket
+from threading import Thread
+import tkinter
+
+def receive():
+    while True:
+        data = c.recv(102).decode("utf8")
+        msg_list.insert(tkinter.END, data)
+
+def send():
+    data = my_msg.get()
+    my_msg.set("")
+    c.send(bytes(data,'utf8'))
+
+
+top = tkinter.Tk()
+top.title("chatbox")
+messages_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()
+msg_list = tkinter.Listbox(messages_frame, height=15, width=50,  bg = 'white')
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+messages_frame.pack()
+
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)/home/ioss/PycharmProjects/untitled/server.py
+entry_field.pack()
+send_button = tkinter.Button(top, text="Send", command=send,fg = 'red')
+send_button.pack()
+
+host = '192.168.1.184'
+port = 12345
+addr = (host,port)
+c = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+c.connect(addr)
+print("enter your name")
+name = input()
+c.send(bytes(name,"utf8"))
+Thread(target = receive).start()
+tkinter.mainloop()
+#while True:
+    #print(name,end = '')
+    #data = input()
+    #c.send(bytes(data,'utf8'))
+
+
+
---
+++ server.py
@@ -0,0 +1,41 @@
+from threading import Thread
+import socket
+addr_client = {}
+clients  = {}
+count = 0
+def incoming_connection():
+    while True:
+        conn, client_addr = s.accept()
+        print(client_addr)
+        #print(conn)
+        addr_client[conn] = client_addr
+        #print (conn)
+        Thread(target = client_connection, args = (conn,)).start()
+
+
+def client_connection(conn):
+    name = conn.recv(100).decode("utf8")
+    clients[conn] = name
+
+    while True:
+        msg = conn.recv(100).decode("utf8")
+        broadcast(msg,name+":")
+
+
+def broadcast(msg,prefix):
+    for sock in clients:
+        #print(prefix)
+        sock.send(bytes(prefix+msg,"utf8"))
+
+
+host = '192.168.1.187'
+port = 12345
+addr = (host,port)
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s.bind(addr)
+
+s.listen(5)
+print ("waiting for connection")
+Thread (target = incoming_connection).start()
+#Thread (target = incoming_connection).join()
